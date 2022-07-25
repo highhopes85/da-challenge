5138c4bf54a5435d303c04d281771d2e28f4d8c9
vincenzo.lalomia <vincenzo.lalomia@gmail.com>
2022-07-25 23:23:35 +0200

    created project

---
+++ src/main/java/it/da/be/challenge/fruit/App.java
@@ -0,0 +1,11 @@
+package it.da.be.challenge.fruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/test/java/it/da/be/challenge/fruit/AppTest.java
@@ -0,0 +1,13 @@
+package it.da.be.challenge.fruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+	@Test
+	void testLoadContext() {
+	}
+
+}

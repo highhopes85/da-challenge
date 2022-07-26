238e4673a8825d3efb074132152deb8d61af122f
vincenzo.lalomia <vincenzo.lalomia@gmail.com>
2022-07-26 18:33:21 +0200

    not nullable columns

--- src/main/java/it/da/be/challenge/fruit/entity/FruitNutritions.java
+++ src/main/java/it/da/be/challenge/fruit/entity/FruitNutritions.java
@@ -1,5 +1,6 @@
 package it.da.be.challenge.fruit.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,61 +11,52 @@ public class FruitNutritions {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false)
 	private double carbohydrates;
+	@Column(nullable = false)
     private double protein;
+	@Column(nullable = false)
     private double fat;
+	@Column(nullable = false)
     private int calories;
+	@Column(nullable = false)
     private double sugar;
-    
+	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public Integer getCalories() {
-		return calories;
-	}
-	public void setCalories(Integer calories) {
-		this.calories = calories;
-	}
-
 	public double getCarbohydrates() {
 		return carbohydrates;
 	}
-
 	public void setCarbohydrates(double carbohydrates) {
 		this.carbohydrates = carbohydrates;
 	}
-
 	public double getProtein() {
 		return protein;
 	}
-
 	public void setProtein(double protein) {
 		this.protein = protein;
 	}
-
 	public double getFat() {
 		return fat;
 	}
-
 	public void setFat(double fat) {
 		this.fat = fat;
 	}
-
-	public double getSugar() {
-		return sugar;
+	public int getCalories() {
+		return calories;
 	}
-
-	public void setSugar(double sugar) {
-		this.sugar = sugar;
-	}
-
 	public void setCalories(int calories) {
 		this.calories = calories;
 	}
-
+	public double getSugar() {
+		return sugar;
+	}
+	public void setSugar(double sugar) {
+		this.sugar = sugar;
+	}
+    
 }
